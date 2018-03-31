66121cc124edcd6afe5d3ba122a41d0fe794a1bf
Jinchi Wei <jwei9@jhu.edu@login-node03.cm.cluster>
2018-03-31 19:35:18 -0400

    Cleaned up sortdataset script

--- misc/sortdataset.py
+++ misc/sortdataset.py
@@ -1,13 +1,8 @@
-#!/bin/bash - unused since this is now Python
-
-# Idea: Take a txt file and 4 directories as arguments.
-# Read the txt file and take file names.
+# Idea: Take source and new image directories as arguments.
 # Move all file names from first directory to second directory, etc.
 # Repeat for second group of files, etc.
 
 import os
-
-# datalistfile = input("List of dataset files: ")
 
 dir1 = input("Source directory: ")
 dir2 = input("New training directory: ")
@@ -20,9 +15,7 @@ num3 = int(input("Number of test files: "))
 onlyfiles = [f for f in os.listdir(dir1) if os.path.isfile(os.path.join(dir1, f))]
 
 # for i in range(6):
-# 	AP_line = AP.read()
-
-# datalist = open(datalistfile, "r")
+# 	AP_line = AP.readline()
 
 for i in range(num1):
 	os.rename(dir1 + "/" + onlyfiles[i], dir2 + "/" + onlyfiles[i])
@@ -33,12 +26,3 @@ for i in range(num1, num1 + num2):
 for i in range(num1 + num2, num1 + num2 + num3):
 	os.rename(dir1 + "/" + onlyfiles[i], dir4 + "/" + onlyfiles[i])
 
-
-# for filename in AP:
-#	try:
-#		filename_noline = filename.replace("\n", "")
-#		os.rename(dir1 + "/" + filename_noline, dir2 + "/" + filename_noline)
-#	except FileNotFoundError:
-#		continue
-
-# datalist.close()
