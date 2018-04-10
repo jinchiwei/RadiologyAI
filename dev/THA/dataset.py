d8c3059bcf0ee904a9f2e9501c6b6eb2fc84a03b
Jinchi Wei <jwei9@jhu.edu@login-node02.cm.cluster>
2018-04-10 01:02:49 -0400

    Changed dataset.py to convert all input types to uint8

--- dev/THA/dataset.py
+++ dev/THA/dataset.py
@@ -43,8 +43,8 @@ class THADataset(Dataset):
     elif len(shape) == 2: # for cases (x, y)
       x = color.gray2rgb(x)
 
-    # in order to make sure we have images NOT in uint16 but in uint8
-    if x.dtype == np.uint16:
+    # in order to make sure we have images in uint8
+    if x.dtype != np.uint8:
       x = x.astype(np.uint8)   
 
     if self.transform:
