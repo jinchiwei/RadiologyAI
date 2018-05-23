4f0ad21fb8cf63fc3a6a43cdd47b6345cd12d1f6
dreamxjei <mrJinch@gmail.com>
2018-05-22 22:01:30 -0400

    Updated models.py

--- dev/THA/models.py
+++ dev/THA/models.py
@@ -37,6 +37,57 @@ def inception_v3_pretrained(n_classes, freeze=True):
 
 
 
+def AlexNet_pretrained(n_classes, freeze=True):
+  model = models.__dict__['alexnet'](pretrained=True)
+  if freeze:
+    for param in model.parameters():
+      param.requires_grad = True
+
+  ## change the last 1000-fc to n_classes
+  num_filters = model.fc.in_features
+  model.fc = nn.Linear(num_filters, n_classes)
+  return model
+
+
+
+def SqueezeNet_pretrained(n_classes, freeze=True):
+  model = models.__dict__['squeezenet'](pretrained=True)
+  if freeze:
+    for param in model.parameters():
+      param.requires_grad = True
+
+  ## change the last 1000-fc to n_classes
+  num_filters = model.fc.in_features
+  model.fc = nn.Linear(num_filters, n_classes)
+  return model
+
+
+def VGGNet_pretrained(n_classes, freeze=True):
+  model = models.__dict__['vgg'](pretrained=True)
+  if freeze:
+    for param in model.parameters():
+      param.requires_grad = True
+
+  ## change the last 1000-fc to n_classes
+  num_filters = model.fc.in_features
+  model.fc = nn.Linear(num_filters, n_classes)
+  return model
+
+
+
+def DenseNet_pretrained(n_classes, freeze=True):
+  model = models.__dict__['densenet'](pretrained=True)
+  if freeze:
+    for param in model.parameters():
+      param.requires_grad = True
+
+  ## change the last 1000-fc to n_classes
+  num_filters = model.fc.in_features
+  model.fc = nn.Linear(num_filters, n_classes)
+  return model
+
+
+
 # def GoogLeNet_pretrained(n_classes, freeze=True):
 #   model = GoogLeNet();
 #   if freeze:
