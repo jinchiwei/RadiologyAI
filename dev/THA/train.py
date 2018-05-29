1cbe813866303a0606da9f7d8e13369dabf47284
Joshua Garza <jgarza6@jhu.edu@login-node01.cm.cluster>
2018-05-29 18:44:29 -0400

    made train.py less verbose

--- dev/THA/train.py
+++ dev/THA/train.py
@@ -98,7 +98,7 @@ def main():
   
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=num_epochs):
-  if args.network == 'resnet18':
+  if args.network == 'resnet18' or args.network == 'alexnet' or args.network == 'squeezenet' or args.network == 'vggnet' or args.network == 'densenet':
     train_data_transform = transforms.Compose([
       transforms.ToPILImage(),
       transforms.Resize((256, 256)),
@@ -138,98 +138,6 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=num_epochs):
       transforms.ToPILImage(),
       transforms.Resize((300, 300)),
       transforms.CenterCrop(299),
-      transforms.ToTensor(),
-      # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-      #                      std=[0.229, 0.224, 0.225]),
-      # transforms.Normalize(mean=[0.4059296, 0.40955055, 0.412535],
-      #                      std=[0.21329397, 0.215493, 0.21677108]),
-    ])
-  elif args.network == 'alexnet':
-    train_data_transform = transforms.Compose([
-      transforms.ToPILImage(),
-      transforms.Resize((256, 256)),
-      transforms.RandomRotation((-5, +5)),
-      transforms.RandomCrop(224),
-      transforms.RandomHorizontalFlip(),
-      transforms.ToTensor(),
-      # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-      #                      std=[0.229, 0.224, 0.225]),
-      # transforms.Normalize(mean=[0.4059296, 0.40955055, 0.412535],
-      #                      std=[0.21329397, 0.215493, 0.21677108]),
-    ])
-    val_data_transform = transforms.Compose([
-      transforms.ToPILImage(),
-      transforms.Resize((256, 256)),
-      transforms.CenterCrop(224),
-      transforms.ToTensor(),
-      # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-      #                      std=[0.229, 0.224, 0.225]),
-      # transforms.Normalize(mean=[0.4059296, 0.40955055, 0.412535],
-      #                      std=[0.21329397, 0.215493, 0.21677108]),
-    ])
-  elif args.network == 'squeezenet':
-    train_data_transform = transforms.Compose([
-      transforms.ToPILImage(),
-      transforms.Resize((256, 256)),
-      transforms.RandomRotation((-5, +5)),
-      transforms.RandomCrop(224),
-      transforms.RandomHorizontalFlip(),
-      transforms.ToTensor(),
-      # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-      #                      std=[0.229, 0.224, 0.225]),
-      # transforms.Normalize(mean=[0.4059296, 0.40955055, 0.412535],
-      #                      std=[0.21329397, 0.215493, 0.21677108]),
-    ])
-    val_data_transform = transforms.Compose([
-      transforms.ToPILImage(),
-      transforms.Resize((256, 256)),
-      transforms.CenterCrop(224),
-      transforms.ToTensor(),
-      # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-      #                      std=[0.229, 0.224, 0.225]),
-      # transforms.Normalize(mean=[0.4059296, 0.40955055, 0.412535],
-      #                      std=[0.21329397, 0.215493, 0.21677108]),
-    ])
-  elif args.network == 'vggnet':
-    train_data_transform = transforms.Compose([
-      transforms.ToPILImage(),
-      transforms.Resize((256, 256)),
-      transforms.RandomRotation((-5, +5)),
-      transforms.RandomCrop(224),
-      transforms.RandomHorizontalFlip(),
-      transforms.ToTensor(),
-      # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-      #                      std=[0.229, 0.224, 0.225]),
-      # transforms.Normalize(mean=[0.4059296, 0.40955055, 0.412535],
-      #                      std=[0.21329397, 0.215493, 0.21677108]),
-    ])
-    val_data_transform = transforms.Compose([
-      transforms.ToPILImage(),
-      transforms.Resize((256, 256)),
-      transforms.CenterCrop(224),
-      transforms.ToTensor(),
-      # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-      #                      std=[0.229, 0.224, 0.225]),
-      # transforms.Normalize(mean=[0.4059296, 0.40955055, 0.412535],
-      #                      std=[0.21329397, 0.215493, 0.21677108]),
-    ])
-  elif args.network == 'densenet':
-    train_data_transform = transforms.Compose([
-      transforms.ToPILImage(),
-      transforms.Resize((256, 256)),
-      transforms.RandomRotation((-5, +5)),
-      transforms.RandomCrop(224),
-      transforms.RandomHorizontalFlip(),
-      transforms.ToTensor(),
-      # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-      #                      std=[0.229, 0.224, 0.225]),
-      # transforms.Normalize(mean=[0.4059296, 0.40955055, 0.412535],
-      #                      std=[0.21329397, 0.215493, 0.21677108]),
-    ])
-    val_data_transform = transforms.Compose([
-      transforms.ToPILImage(),
-      transforms.Resize((256, 256)),
-      transforms.CenterCrop(224),
       transforms.ToTensor(),
       # transforms.Normalize(mean=[0.485, 0.456, 0.406],
       #                      std=[0.229, 0.224, 0.225]),
