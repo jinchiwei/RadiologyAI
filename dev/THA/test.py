974e42852ceb1c89f1e53eae27091521fab0ebcb
Jinchi Wei <jwei@lambda>
2018-06-10 15:12:38 -0400

    Fixed test error

--- dev/THA/test.py
+++ dev/THA/test.py
@@ -53,8 +53,8 @@ def main():
             load_file = 'weights_resnet18/resnet18_weights/' + weightslist[weightfile]
             val_data_transform = transforms.Compose([
               transforms.ToPILImage(),
-              #transforms.Resize((256, 256)),
-              #transforms.CenterCrop(224),
+              transforms.Resize((256, 256)),
+              transforms.CenterCrop(224),
               transforms.ToTensor(),
               # transforms.Normalize(mean=[0.485, 0.456, 0.406],
               #                      std=[0.229, 0.224, 0.225]),
