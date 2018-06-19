9c6346f5dfe2ed3ab3987098064b0f653b07d517
Jinchi Wei <jwei@lambda>
2018-06-19 17:58:31 -0400

    Made resnet50 the default selection

--- dev/THA/test.py
+++ dev/THA/test.py
@@ -34,7 +34,7 @@ from sklearn import metrics
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', '--network',
-        choices=['resnet18', 'resnet50', 'inception_v3', 'alexnet', 'squeezenet', 'vggnet', 'densenet'], default='resnet18',
+        choices=['resnet18', 'resnet50', 'inception_v3', 'alexnet', 'squeezenet', 'vggnet', 'densenet'], default='resnet50',
         help='Choose which neural network to use')
     args = parser.parse_args()
     network = args.network
