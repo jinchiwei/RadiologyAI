d0d7b5e76adac8586a3cb270c683df0522135391
dreamxjei <mrJinch@gmail.com>
2018-09-14 12:07:18 -0400

    updated for new marcc

--- dev/THA/task_train.sh
+++ dev/THA/task_train.sh
@@ -11,9 +11,9 @@
 #sbatch --mail-type=end
 #sbatch --mail-user=jwei9@jhu.edu
 
-module load cuda cudnn/5.1 gcc slurm feh anaconda-python/3.6
+module load cuda cudnn/7.2.1 gcc python/anaconda-3.6
 module unload parallel_studio_xe_2015/15.0.2
 
-source activate deeplearning
-python train.py -n inception_v3
+source activate deepdream
+python train.py
 source deactivate
